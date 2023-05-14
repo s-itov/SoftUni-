72f0c6b4eaec51473a871cd5cc8a7622d6e115b1
Simeon Itov <simeon.itov@gmail.com>
2023-05-14 13:40:30 +0300

    separate function and module exports it

--- JS Back-End/cat-shelter/handlers/home.js
+++ JS Back-End/cat-shelter/handlers/home.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const cats = require('../data/cats.json')
 const breed = require('../data/breeds.json')
+const getContentType = require('./contentType');
+
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
@@ -18,14 +20,14 @@ module.exports = (req, res) => {
     fs.readFile(filePath, (err, data) => {
         try {
             if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.writeHead(404, { 'Content-Type': getContentType(filePath) });
                 res.end('404 Not Found');
             } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.writeHead(200, { 'Content-Type': getContentType(filePath)});
                 res.end(data);
             }
         } catch (error) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.writeHead(500, { 'Content-Type': getContentType(filePath) });
             res.end('500 Internal Server Error');
         }
     });
