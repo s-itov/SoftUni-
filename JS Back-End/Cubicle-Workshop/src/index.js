f81f8cfa893d4d0ab8383ded5e06b394ea8d329b
Simeon Itov <simeon.itov@gmail.com>
2023-05-23 20:35:01 +0300

    add handlebars to project

--- JS Back-End/Cubicle-Workshop/src/index.js
+++ JS Back-End/Cubicle-Workshop/src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+
+const PORT = 3000;
+
+//Handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.listen(PORT, () => console.log(`Server is runnning on port ${PORT}...`));
