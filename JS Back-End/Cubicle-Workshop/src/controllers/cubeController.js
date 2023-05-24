fb03a85835aee46c524a754c1aca2713ab67cd19
Simeon Itov <simeon.itov@gmail.com>
2023-05-24 10:30:36 +0300

    adding cubeManager with create Logic

--- JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
+++ JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
+const cubeManager = require('../managers/cubeManager');
 
 router.get('/create', (req, res) => {
     res.render('create');
+    console.log(cubeManager.getAll());
 });
 
 router.post('/create', (req, res) => {
-    console.log(req.body);
+
+    const {
+        name,
+        description,
+        imageUrl,
+        difficultyLevel
+    } = req.body;
+
+    cubeManager.create({
+        name,
+        description,
+        imageUrl,
+        difficultyLevel: Number(difficultyLevel),
+    });
 
     res.redirect('/');
 });
