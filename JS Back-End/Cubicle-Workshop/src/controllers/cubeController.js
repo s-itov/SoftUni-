5fd87c711ab5b777d670ff38fc2109504b575f8b
Simeon Itov <simeon.itov@gmail.com>
2023-05-29 22:08:14 +0300

    creating new method for showing remaining accessories

--- JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
+++ JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
@@ -38,7 +38,7 @@ router.get('/:cubeId/details', async (req, res) => {
 
 router.get('/:cubeId/attach', async (req, res) => {
     const cube =  await cubeManager.getOne(req.params.cubeId).lean();
-    const accessories = await accessoryManager.getAll(cube).lean();
+    const accessories = await accessoryManager.showRemainingAccessories(cube).lean();
 
     res.render('attachAccessory', { cube, accessories });
 });
--- JS Back-End/Cubicle-Workshop/src/managers/accessoryManager.js
+++ JS Back-End/Cubicle-Workshop/src/managers/accessoryManager.js
@@ -2,4 +2,6 @@ const Accessory = require('../models/Accessory');
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
 
-exports.getAll = (cube) =>  Accessory.find({ _id: {$nin: cube.accessories}});
+exports.getAll = () =>  Accessory.find();
+
+exports.showRemainingAccessories = (cube) =>  Accessory.find({ _id: {$nin: cube.accessories}});
