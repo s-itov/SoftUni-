becf2a905c2c60365795b66395fda025a80d5b52
Simeon Itov <simeon.itov@gmail.com>
2023-05-31 19:49:43 +0300

    adding new resources

--- JS Back-End/Cubicle-Workshop/src/controllers/homeController.js
+++ JS Back-End/Cubicle-Workshop/src/controllers/homeController.js
@@ -14,11 +14,6 @@ router.get('/about', (req, res) => {
     res.render('about');
 })
 
-router.get('/create', (req, res) => {
-    res.render('create');
-})
-
-
 router.get('/404', (req, res) => {
     res.render('404');
 })
