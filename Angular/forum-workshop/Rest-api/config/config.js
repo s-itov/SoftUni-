fbcb6660e950a4ecad64f4b5a150423118b036b2
Simeon Itov <simeon.itov@gmail.com>
2023-07-05 22:39:27 +0300

    configuring server to run

--- Angular/forum-workshop/Rest-api/config/config.js
+++ Angular/forum-workshop/Rest-api/config/config.js
@@ -3,8 +3,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        dbURL: 'mongodb://localhost:27017/forum',
-        origin: ['http://localhost:5555', 'http://localhost:4200']
+        dbURL: 'mongodb://127.0.0.1:27017/forum',
+        origin: ['http://127.0.0.1:5555', 'http://127.0.0.1:4200']
     },
     production: {
         port: process.env.PORT || 3000,
