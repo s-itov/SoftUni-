62291a1de7478f3b0af41b02f0cf87bc003b57f4
s-itov <simeon.itov@gmail.com>
2023-07-07 09:34:42 +0300

    implementing services

--- Angular/forum-workshop/forum-app/src/app/api.service.ts
+++ Angular/forum-workshop/forum-app/src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,14 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts() {}
-  getThemes() {}
+  getPosts(limit?: number) {
+    const { appUrl } = environment;
+    const limitFilter = limit ? `?limit=${limit}` : '';
+    return this.http.get(`${appUrl}/posts${limitFilter}`);
+  }
+  getThemes() {
+
+    const { appUrl } = environment;
+    return this.http.get(`${appUrl}/posts`);
+  }
 }
