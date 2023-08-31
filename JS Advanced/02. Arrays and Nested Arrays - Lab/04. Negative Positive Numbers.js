f5f17520ab1c1f3bd8d564523c20a6d54b6d2d36
s-itov <simeon.itov@gmail.com>
2023-08-31 16:28:47 +0300

    adding solution to task 04.

---
+++ JS Advanced/02. Arrays and Nested Arrays - Lab/04. Negative Positive Numbers.js
@@ -0,0 +1,19 @@
+function solve(array){
+
+    let result = []
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] < 0) {
+            result.unshift(array[i]);
+        } else {
+            result.push(array[i])
+        }
+        
+    }
+
+    console.log(result.join('\n'));
+
+}
+
+solve([7, -2, 8, 9]);
